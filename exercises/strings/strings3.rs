869f8458bd8142ce65bb886abbdfd318d2b5ba25
rabibit <rabibit@163.com>
2023-10-22 17:31:40 +0800

    finished 64

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,26 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut s = String::from(input);
+    let end_offset = input.find('s').unwrap_or(input.len())+1;
+    let st_offset = input.find('c').unwrap_or(input.len());
+
+    // Replace the range up until the β from the string
+    s.replace_range(st_offset..end_offset, "balloons");
+    s
 }
 
 #[cfg(test)]
